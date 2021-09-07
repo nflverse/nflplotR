bd18362d6035cc367f0ee043e1249f03b87a06e7
mrcaseb <roemerle@gmail.com>
2021-09-07 17:57:31 +0200

    add conf logos

--- data-raw/build_logo_list.R
+++ data-raw/build_logo_list.R
@@ -1,5 +1,12 @@
 # Save raw logos in internal data for more speed
-teams_colors_logos <- nflreadr::load_teams()
+teams_colors_logos <- nflreadr::load_teams() |>
+  dplyr::bind_rows(
+    tibble::tibble(
+      team_abbr = c("AFC", "NFC"),
+      team_logo_espn = c("https://github.com/nflverse/nflplotR/raw/main/data-raw/AFC.png",
+                         "https://github.com/nflverse/nflplotR/raw/main/data-raw/NFC.png")
+    )
+  )
 
 logo_list <- lapply(teams_colors_logos$team_abbr, function(x){
   url <- teams_colors_logos$team_logo_espn[teams_colors_logos$team_abbr == x]
@@ -9,4 +16,3 @@ logo_list <- lapply(teams_colors_logos$team_abbr, function(x){
 logo_list <- rlang::set_names(logo_list, teams_colors_logos$team_abbr)
 
 use_data(logo_list, internal = TRUE, overwrite = TRUE)
-
