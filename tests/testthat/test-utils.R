8166f10fb40d25b3a53ce00bb2fd318139ca4f83
mrcaseb <roemerle@gmail.com>
2021-09-07 14:46:31 +0200

    add tests and update NAMESPACE

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,12 @@
+test_that("valid_team_names works", {
+  valid_all <- c("ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE",
+                 "DAL", "DEN", "DET", "GB", "HOU", "IND", "JAX", "KC",
+                 "LA", "LAC", "LAR", "LV", "MIA", "MIN", "NE", "NO",
+                 "NYG", "NYJ", "OAK", "PHI", "PIT", "SD", "SEA", "SF",
+                 "STL", "TB", "TEN", "WAS")
+
+  valid_filtered <- valid_all[!valid_all %in% c("LAR", "OAK", "SD", "STL")]
+
+  expect_identical(valid_team_names(exclude_duplicates = TRUE), valid_filtered)
+  expect_identical(valid_team_names(exclude_duplicates = FALSE), valid_all)
+})
