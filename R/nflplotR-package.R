42d20faf6a033ee1c71ac41f3bef72886c43c95d
mrcaseb <roemerle@gmail.com>
2022-06-03 17:40:14 +0200

    add DT dependency

--- R/nflplotR-package.R
+++ R/nflplotR-package.R
@@ -4,6 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @import data.table
 #' @importFrom ggplot2 element_grob
 #' @import grid
 #' @importFrom magrittr %>%
