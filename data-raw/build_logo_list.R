43ac16ec39b21a445f494604efcb2f75fa317dda
Sebastian Carl <mrcaseb@gmail.com>
2024-01-23 10:55:29 +0100

    add wordmark urls to sysdata

--- data-raw/build_logo_list.R
+++ data-raw/build_logo_list.R
@@ -18,12 +18,19 @@ logo_list <- rlang::set_names(logo_list, teams_colors_logos$team_abbr)
 
 primary_colors <- teams_colors_logos$team_color
 names(primary_colors) <- teams_colors_logos$team_abbr
+primary_colors <- primary_colors[!is.na(primary_colors)]
 
 secondary_colors <- teams_colors_logos$team_color2
 names(secondary_colors) <- teams_colors_logos$team_abbr
+secondary_colors <- secondary_colors[!is.na(secondary_colors)]
 
 logo_urls <- teams_colors_logos$team_logo_espn
 names(logo_urls) <- teams_colors_logos$team_abbr
+logo_urls <- logo_urls[!is.na(logo_urls)]
+
+wordmark_urls <- teams_colors_logos$team_wordmark
+names(wordmark_urls) <- teams_colors_logos$team_abbr
+wordmark_urls <- wordmark_urls[!is.na(wordmark_urls)]
 
 # Save raw logos in internal data for more speed
 wordmarks <- nflreadr::load_teams() |> dplyr::select(team_abbr, team_wordmark)
@@ -33,4 +40,4 @@ wordmark_list <- lapply(wordmarks$team_wordmark, function(url){
 
 wordmark_list <- rlang::set_names(wordmark_list, wordmarks$team_abbr)
 
-usethis::use_data(logo_list, primary_colors, secondary_colors, logo_urls, wordmark_list, internal = TRUE, overwrite = TRUE)
+usethis::use_data(logo_list, primary_colors, secondary_colors, logo_urls, wordmark_urls, wordmark_list, internal = TRUE, overwrite = TRUE)
