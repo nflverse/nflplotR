40f545f570e055d77db1ff3f7084fedcf7d59109
mrcaseb <mrcaseb@gmail.com>
2024-02-04 14:09:04 +0100

    describe formats all items bold so we need a new way to highlight required aesthetics

--- R/geom_nfl_wordmarks.R
+++ R/geom_nfl_wordmarks.R
@@ -6,11 +6,11 @@
 #'
 #' @inheritParams ggplot2::geom_point
 #' @section Aesthetics:
-#' `geom_nfl_logos()` understands the following aesthetics (required aesthetics are in bold):
+#' `geom_nfl_logos()` understands the following aesthetics (required aesthetics have no default value):
 #' \describe{
-#'   \item{**`x`**}{The x-coordinate.}
-#'   \item{**`y`**}{The y-coordinate.}
-#'   \item{**`team_abbr`**}{The team abbreviation. Should be one of [`valid_team_names()`]. The function tries to clean team names internally by calling [`nflreadr::clean_team_abbrs()`]}
+#'   \item{`x`}{The x-coordinate. Required.}
+#'   \item{`y`}{The y-coordinate. Required.}
+#'   \item{`team_abbr`}{The team abbreviation. Should be one of [`valid_team_names()`]. The function tries to clean team names internally by calling [`nflreadr::clean_team_abbrs()`]. Required.}
 #'   \item{`alpha = NULL`}{The alpha channel, i.e. transparency level, as a numerical value between 0 and 1.}
 #'   \item{`colour = NULL`}{The image will be colorized with this colour. Use the special character `"b/w"` to set it to black and white. For more information on valid colour names in ggplot2 see <https://ggplot2.tidyverse.org/articles/ggplot2-specs.html?q=colour#colour-and-fill>}
 #'   \item{`angle = 0`}{The angle of the image as a numerical value between 0° and 360°.}
