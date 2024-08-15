80e9f8c36e20b1b3153963f1902043a28d4436ba
Sebastian Carl <mrcaseb@gmail.com>
2024-08-15 16:47:23 +0200

    implement conditional import

--- R/nflplotR-package.R
+++ R/nflplotR-package.R
@@ -12,5 +12,10 @@
 #' @importFrom gt %>%
 #' @importFrom rlang .data %||%
 #' @importFrom utils capture.output
+#' @rawNamespace if (getRversion() >= "4.0.0") {
+#'   importFrom(tools, R_user_dir)
+#' } else {
+#'   importFrom(backports, R_user_dir)
+#' }
 ## usethis namespace: end
 NULL
